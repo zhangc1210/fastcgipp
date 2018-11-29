4ac0c66e818e9f0b7e001471149b57a345ccfb8e
Eddie Carle <eddie@isatec.ca>
2018-11-28 19:32:04 -0700

    Added Mailer class for sending emails

---
+++ include/fastcgi++/mailer.hpp
@@ -0,0 +1,196 @@
+/*!
+ * @file       mailer.hpp
+ * @brief      Declares types for sending emails
+ * @author     Eddie Carle &lt;eddie@isatec.ca&gt;
+ * @date       November 28, 2018
+ * @copyright  Copyright &copy; 2018 Eddie Carle. This project is released under
+ *             the GNU Lesser General Public License Version 3.
+ */
+/*******************************************************************************
+* Copyright (C) 2018 Eddie Carle [eddie@isatec.ca]                             *
+*                                                                              *
+* This file is part of fastcgi++.                                              *
+*                                                                              *
+* fastcgi++ is free software: you can redistribute it and/or modify it under   *
+* the terms of the GNU Lesser General Public License as  published by the Free *
+* Software Foundation, either version 3 of the License, or (at your option)    *
+* any later version.                                                           *
+*                                                                              *
+* fastcgi++ is distributed in the hope that it will be useful, but WITHOUT ANY *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    *
+* FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for     *
+* more details.                                                                *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with fastcgi++.  If not, see <http://www.gnu.org/licenses/>.           *
+*******************************************************************************/
+
+#ifndef FASTCGIPP_MAILER_HPP
+#define FASTCGIPP_MAILER_HPP
+
+#include <queue>
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+#include "fastcgi++/sockets.hpp"
+#include "fastcgi++/email.hpp"
+
+//! Topmost namespace for the fastcgi++ library
+namespace Fastcgipp
+{
+    //! Contains the stuff for composing and sending emails
+    namespace Mail
+    {
+        //! Handles low level communication with an SMTP server
+        /*!
+         * This class handles connections to the SMTP and it's queue of emails.
+         * It can safely be constructed in the global space. Once constructed
+         * call init() to initialize it. Call start() to start the handling
+         * thread. Once you're done, call stop() or terminate() to finish things
+         * and then call join() to wait for the thread to complete.
+         *
+         * Queue up emails for sending with queue().
+         *
+         * @date    November 28, 2018
+         * @author  Eddie Carle &lt;eddie@isatec.ca&gt;
+         */
+        class Mailer
+        {
+        public:
+            //! Call from any thread to stop the handler() thread
+            /*!
+             * Calling this thread will signal the handler() thread to
+             * gracefully stop itself. This means it waits until all
+             * queued queries are finished
+             *
+             * @sa join()
+             */
+            void stop();
+
+            //! Call from any thread to terminate the handler() thread
+            /*!
+             * Calling this thread will signal the handler() thread to
+             * immediately terminate itself. This means it doesn't wait until
+             * the currently queued queries are finished.
+             *
+             * @sa join()
+             */
+            void terminate();
+
+            //! Call from any thread to start the handler() thread
+            /*!
+             * If the thread is already running this will do nothing.
+             */
+            void start();
+
+            //! %Block until a stop() or terminate() is completed
+            void join();
+
+            //! Queue up an email
+            void queue(Email_base& email);
+
+            //! Initialize the mailer
+            /*!
+             * Note that this function can only be called _once_.
+             *
+             * @param [in] host Hostname/address of the SMTP server.
+             * @param [in] origin Hostname/address of origin.
+             * @param [in] port Server port number
+             * @param [in] retryInterval How many seconds before retrying a bad
+             *                           connection to the SMTP server?
+             */
+            void init(
+                    const char* host,
+                    const char* origin,
+                    const unsigned short port=25,
+                    unsigned retryInterval=30);
+
+            ~Mailer();
+
+            Mailer():
+                m_initialized(false),
+                m_state(DISCONNECTED)
+            {}
+
+        private:
+            //! General mailer handler
+            void handler();
+
+            //! True if we're initialized
+            bool m_initialized;
+
+            //! %Buffer for transmitting data
+            std::queue<Email_base::Data> m_queue;
+
+            //! True when handler() should be terminating
+            std::atomic_bool m_terminate;
+
+            //! True when handler() should be stopping
+            std::atomic_bool m_stop;
+
+            //! Thread our handler is running in
+            std::thread m_thread;
+
+            //! Always practice safe threading
+            std::mutex m_mutex;
+
+            //! Hostname/address of server
+            std::string m_host;
+
+            //! Origin hostname
+            std::string m_origin;
+
+            //! Server port
+            std::string m_port;
+
+            //! %Mailer retry interval
+            unsigned m_retry;
+
+            //! The socket group
+            SocketGroup m_socketGroup;
+
+            //! The connection socket
+            Socket m_socket;
+
+            //! Current email
+            Email_base::Data m_email;
+
+            //! Are we currently working on an email?
+            bool inEmail() const
+            {
+                return !m_email.to.empty();
+            }
+
+            //! Purge current email
+            void purgeEmail()
+            {
+                m_email.body.clear();
+                m_email.to.clear();
+                m_email.from.clear();
+            }
+
+            //! State enumeration
+            enum State
+            {
+                DISCONNECTED,
+                CONNECTED,
+                EHLO,
+                EIGHTBIT,
+                MAIL,
+                RCPT,
+                DATA,
+                DUMP,
+                QUIT
+            };
+
+            //! Current state
+            State m_state;
+
+            //! Current line being read from SMTP server
+            std::string m_line;
+        };
+    }
+}
+
+#endif
---
+++ src/mailer.cpp
@@ -0,0 +1,346 @@
+/*!
+ * @file       mailer.cpp
+ * @brief      Defines types for sending emails
+ * @author     Eddie Carle &lt;eddie@isatec.ca&gt;
+ * @date       November 28, 2018
+ * @copyright  Copyright &copy; 2018 Eddie Carle. This project is released under
+ *             the GNU Lesser General Public License Version 3.
+ */
+/*******************************************************************************
+* Copyright (C) 2018 Eddie Carle [eddie@isatec.ca]                             *
+*                                                                              *
+* This file is part of fastcgi++.                                              *
+*                                                                              *
+* fastcgi++ is free software: you can redistribute it and/or modify it under   *
+* the terms of the GNU Lesser General Public License as  published by the Free *
+* Software Foundation, either version 3 of the License, or (at your option)    *
+* any later version.                                                           *
+*                                                                              *
+* fastcgi++ is distributed in the hope that it will be useful, but WITHOUT ANY *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    *
+* FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for     *
+* more details.                                                                *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with fastcgi++.  If not, see <http://www.gnu.org/licenses/>.           *
+*******************************************************************************/
+
+#include "fastcgi++/mailer.hpp"
+#include "fastcgi++/log.hpp"
+
+void Fastcgipp::Mail::Mailer::handler()
+{
+    while(!m_terminate && !(m_stop && m_queue.empty() && !inEmail()))
+    {
+        if(!inEmail() && !m_queue.empty())
+        {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            m_email = std::move(m_queue.front());
+            m_queue.pop();
+        }
+
+        if(inEmail() && !m_socket.valid())
+        {
+            m_line.clear();
+            m_state = CONNECTED;
+            m_socket = m_socketGroup.connect(m_host.c_str(), m_port.c_str());
+        }
+
+        if(m_socketGroup.poll(true) == m_socket)
+        {
+            char buffer;
+            while(m_socket.read(&buffer, 1) == 1)
+            {
+                if(buffer == '\n')
+                    break;
+                m_line.push_back(buffer);
+            }
+
+            if(buffer == '\n')
+            {
+                if(m_line.back() == '\r')
+                    m_line.pop_back();
+
+                switch(m_state)
+                {
+                    case CONNECTED:
+                    {
+                        if(m_line.size() >= 4 && m_line.substr(0,4) == "220 ")
+                        {
+                            m_line = "EHLO ";
+                            m_line += m_origin + '\n';
+                            if(m_socket.write(m_line.data(), m_line.size())
+                                    != m_line.size())
+                            {
+                                ERROR_LOG("Error sending EHLO command to SMTP "\
+                                        "server.")
+                            }
+                            else
+                            {
+                                m_state = EHLO;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after "\
+                                    "connecting: " << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+
+                    case EHLO:
+                    {
+                        if(m_line == "250-EIGHTBITMIME")
+                        {
+                            m_state == EIGHTBIT;
+                            break;
+                        }
+                        else if(m_line.size()>=4 && m_line.substr(0,3)=="250")
+                        {
+                            if(m_line[3] == ' ')
+                            {
+                                ERROR_LOG("SMTP server does not support EIGHTBIT.")
+                            }
+                            else if(m_line[3] == '-')
+                                break;
+                            else
+                                ERROR_LOG("Bad reply from SMTP server after "\
+                                        "EHLO: " << m_line.c_str())
+                        }
+                        else
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after EHLO: "\
+                                    << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+
+                    case EIGHTBIT:
+                    {
+                        if(m_line.size()>=4 && m_line.substr(0,3)=="250")
+                        {
+                            if(m_line[3] == ' ')
+                            {
+                                m_line = "MAIL FROM: <";
+                                m_line += m_email.from + ">\n";
+                                if(m_socket.write(m_line.data(), m_line.size())
+                                        != m_line.size())
+                                {
+                                    ERROR_LOG("Error sending MAIL command to "\
+                                            "SMTP server.")
+                                }
+                                else
+                                {
+                                    m_state = MAIL;
+                                    break;
+                                }
+                            }
+                            else if(m_line[3] == '-')
+                                break;
+                        }
+                        else
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after EHLO: "\
+                                    << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+
+                    case MAIL:
+                    {
+                        if(m_line.size() >= 4 && m_line.substr(0,4) == "250 ")
+                        {
+                            m_line = "RCPT TO: <";
+                            m_line += m_email.to + ">\n";
+                            if(m_socket.write(m_line.data(), m_line.size())
+                                    != m_line.size())
+                            {
+                                ERROR_LOG("Error sending RCPT command to SMTP "\
+                                        "server.")
+                            }
+                            else
+                            {
+                                m_state = RCPT;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after MAIL: "\
+                                    << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+
+                    case RCPT:
+                    {
+                        if(m_line.size() >= 4 && m_line.substr(0,4) == "250 ")
+                        {
+                            if(m_socket.write("DATA\n", 5) != 5)
+                            {
+                                ERROR_LOG("Error sending DATA command to SMTP "\
+                                        "server.")
+                            }
+                            else
+                            {
+                                m_state = DATA;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after RCPT: "\
+                                    << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+
+                    case DATA:
+                    {
+                        if(m_line.size() >= 4 && m_line.substr(0,4) == "354 ")
+                        {
+                            bool success=true;
+                            for(const auto& chunk: m_email.body)
+                                if(m_socket.write(
+                                            chunk.data.get(),
+                                            chunk.size) != chunk.size)
+                                {
+                                    ERROR_LOG("Error sending data chunk "\
+                                            "to SMTP server.")
+                                    success=false;
+                                    break;
+                                }
+                            if(success)
+                            {
+                                if(m_socket.write("\r\n.\r\n", 5) != 5)
+                                {
+                                    ERROR_LOG("Error sending CRLF.CRLF to "\
+                                            "SMTP server.")
+                                }
+                                else
+                                {
+                                    m_state = DUMP;
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after RCPT: "\
+                                    << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+
+                    case DUMP:
+                    {
+                        if(m_line.size() >= 4 && m_line.substr(0,4) == "250 ")
+                        {
+                            purgeEmail();
+                            if(m_socket.write("QUIT\n", 5) != 5)
+                            {
+                                ERROR_LOG("Error sending QUIT command to SMTP "\
+                                        "server.")
+                            }
+                            else
+                            {
+                                m_state = QUIT;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after data "\
+                                    "insertion: " << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+
+                    case QUIT:
+                    {
+                        if(!(m_line.size() >= 4 && m_line.substr(0,4) == "221 "))
+                        {
+                            ERROR_LOG("Bad reply from SMTP server after QUIT: "\
+                                    << m_line.c_str())
+                        }
+                        m_socket.close();
+                        m_state = DISCONNECTED;
+                        break;
+                    }
+                }
+                m_line.clear();
+            }
+        }
+    }
+}
+
+void Fastcgipp::Mail::Mailer::stop()
+{
+    m_stop=true;
+    m_socketGroup.wake();
+}
+
+void Fastcgipp::Mail::Mailer::terminate()
+{
+    m_terminate=true;
+    m_socketGroup.wake();
+}
+
+void Fastcgipp::Mail::Mailer::start()
+{
+    if(!m_thread.joinable())
+    {
+        m_stop=false;
+        m_terminate=false;
+        std::thread thread(&Fastcgipp::Mail::Mailer::handler, this);
+        m_thread.swap(thread);
+    }
+}
+
+void Fastcgipp::Mail::Mailer::join()
+{
+    if(m_thread.joinable())
+        m_thread.join();
+}
+
+Fastcgipp::Mail::Mailer::~Mailer()
+{
+}
+
+void Fastcgipp::Mail::Mailer::init(
+        const char* host,
+        const char* origin,
+        const unsigned short port,
+        unsigned retryInterval)
+{
+    if(!m_initialized)
+    {
+        m_host = host;
+        m_origin = origin;
+        m_port = std::to_string(port);
+        m_retry = retryInterval*1000;
+        m_initialized = true;
+    }
+}
+
+void Fastcgipp::Mail::Mailer::queue(Email_base& email)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    m_queue.push(email.data());
+    m_socketGroup.wake();
+}
