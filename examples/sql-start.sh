0410e1d8b8b9804b25c372bc019cdc8654e467ce
Eddie Carle <eddie@isatec.ca>
2019-05-12 17:02:45 -0600

    Adding SQL password to example setup script

--- examples/sql-start.sh
+++ examples/sql-start.sh
@@ -2,4 +2,5 @@
 
 psql -d "postgres" -c "CREATE ROLE fastcgipp_example NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN PASSWORD 'fastcgipp_example';" &&
 createdb -O fastcgipp_example fastcgipp_example &&
+export PGPASSWORD="fastcgipp_example"
 psql -U fastcgipp_example -d fastcgipp_example -c "CREATE TABLE fastcgipp_example (stamp timestamp, address inet, string text);"
